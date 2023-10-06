2ae76b236eff126608c8bc617bfd5c113fa85383
Not An Agency! <119770944+notanagency@users.noreply.github.com>
2023-10-06 12:58:19 -0500

    Update and rename countdown.js to ml-countdown.js
    Multilanguage Countdown

---
+++ ml-countdown.js
@@ -0,0 +1,332 @@
+const _0x4b0841 = _0x17a2;
+(function (_0x231370, _0x1a5df1) {
+  const _0x12f7c5 = _0x17a2,
+    _0x4e6171 = _0x231370();
+  while (!![]) {
+    try {
+      const _0x362f40 =
+        (parseInt(_0x12f7c5(0x15f)) / 0x1) *
+          (parseInt(_0x12f7c5(0x12c)) / 0x2) +
+        -parseInt(_0x12f7c5(0x145)) / 0x3 +
+        (-parseInt(_0x12f7c5(0x15d)) / 0x4) *
+          (parseInt(_0x12f7c5(0x157)) / 0x5) +
+        (-parseInt(_0x12f7c5(0x129)) / 0x6) *
+          (-parseInt(_0x12f7c5(0x138)) / 0x7) +
+        (-parseInt(_0x12f7c5(0x14a)) / 0x8) *
+          (parseInt(_0x12f7c5(0x126)) / 0x9) +
+        (parseInt(_0x12f7c5(0x136)) / 0xa) *
+          (-parseInt(_0x12f7c5(0x13c)) / 0xb) +
+        (-parseInt(_0x12f7c5(0x14b)) / 0xc) *
+          (-parseInt(_0x12f7c5(0x139)) / 0xd);
+      if (_0x362f40 === _0x1a5df1) break;
+      else _0x4e6171["push"](_0x4e6171["shift"]());
+    } catch (_0x1084d7) {
+      _0x4e6171["push"](_0x4e6171["shift"]());
+    }
+  }
+})(_0x82fb, 0x9c997);
+const _0x38a0d2 = (function () {
+    let _0x42f36a = !![];
+    return function (_0x3b1f34, _0x380413) {
+      const _0x496d2d = _0x42f36a
+        ? function () {
+            const _0x56a800 = _0x17a2;
+            if (_0x380413) {
+              const _0x44cafb = _0x380413[_0x56a800(0x161)](
+                _0x3b1f34,
+                arguments
+              );
+              return (_0x380413 = null), _0x44cafb;
+            }
+          }
+        : function () {};
+      return (_0x42f36a = ![]), _0x496d2d;
+    };
+  })(),
+  _0x2a9fad = _0x38a0d2(this, function () {
+    const _0xadbc32 = _0x17a2;
+    return _0x2a9fad[_0xadbc32(0x12b)]()
+      [_0xadbc32(0x120)]("(((.+)+)+)+$")
+      [_0xadbc32(0x12b)]()
+      [_0xadbc32(0x12e)](_0x2a9fad)
+      ["search"](_0xadbc32(0x133));
+  });
+function _0x82fb() {
+  const _0x3c026a = [
+    "prototype",
+    "hours",
+    "querySelector",
+    "json",
+    "(((.+)+)+)+$",
+    "bind",
+    "assign",
+    "2717210hMtYks",
+    "fc-ml-countdown",
+    "8411809FJDcfU",
+    "147953gCshuA",
+    "padStart",
+    "return\x20(function()\x20",
+    "11xaNCwg",
+    "textContent",
+    "getAttribute",
+    "floor",
+    "catch",
+    "getTime",
+    "घंटे",
+    "Std",
+    "గంటలు",
+    "2151357sskyUR",
+    "display",
+    "मिनट",
+    "ساعات",
+    "ঘন্টা",
+    "68064PhguzK",
+    "1524qmDCDz",
+    "fc-countdown-language",
+    "style",
+    "https://worldtimeapi.org/api/timezone/Etc/UTC",
+    "status",
+    "table",
+    "[fc-ml-countdown]",
+    "ਘੰਟੇ",
+    "setLanguageLabels",
+    "exception",
+    "Sek",
+    "HTTP\x20error!\x20Status:\x20",
+    "1973905OiTIGC",
+    "সেকেন্ড",
+    "minutes",
+    "ਮਿੰਟ",
+    "then",
+    "DOMContentLoaded",
+    "8eAONqI",
+    "Error\x20fetching\x20time:",
+    "47wFibHx",
+    "trace",
+    "apply",
+    "ثواني",
+    "length",
+    "warn",
+    "Min",
+    "fc-ml-countdown-end",
+    "ਦਿਨ",
+    "Element\x20with\x20[fc-ml-countdown]\x20not\x20found.",
+    "seconds",
+    "body",
+    "सेकंड",
+    "মিনিট",
+    "دقائق",
+    "__proto__",
+    "search",
+    "datetime",
+    "error",
+    "సెకన్లు",
+    "రోజు",
+    "দিন",
+    "387xCKYPS",
+    "{}.constructor(\x22return\x20this\x22)(\x20)",
+    "दिन",
+    "6dQiGcs",
+    "console",
+    "toString",
+    "5898SuXxOf",
+    "ਸੈਕਿੰਡ",
+    "constructor"
+  ];
+  _0x82fb = function () {
+    return _0x3c026a;
+  };
+  return _0x82fb();
+}
+_0x2a9fad();
+function _0x17a2(_0xa751b7, _0x6d799c) {
+  const _0x224b15 = _0x82fb();
+  return (
+    (_0x17a2 = function (_0x14f97d, _0x5d357f) {
+      _0x14f97d = _0x14f97d - 0x11f;
+      let _0x5d8661 = _0x224b15[_0x14f97d];
+      return _0x5d8661;
+    }),
+    _0x17a2(_0xa751b7, _0x6d799c)
+  );
+}
+const _0x5d357f = (function () {
+    let _0x5d3a23 = !![];
+    return function (_0x14ce37, _0xb2e782) {
+      const _0x7e5184 = _0x5d3a23
+        ? function () {
+            const _0x30b3c3 = _0x17a2;
+            if (_0xb2e782) {
+              const _0x114419 = _0xb2e782[_0x30b3c3(0x161)](
+                _0x14ce37,
+                arguments
+              );
+              return (_0xb2e782 = null), _0x114419;
+            }
+          }
+        : function () {};
+      return (_0x5d3a23 = ![]), _0x7e5184;
+    };
+  })(),
+  _0x14f97d = _0x5d357f(this, function () {
+    const _0x49585c = _0x17a2,
+      _0x466f8e = function () {
+        const _0x2d509a = _0x17a2;
+        let _0x11a863;
+        try {
+          _0x11a863 = Function(_0x2d509a(0x13b) + _0x2d509a(0x127) + ");")();
+        } catch (_0x1e343d) {
+          _0x11a863 = window;
+        }
+        return _0x11a863;
+      },
+      _0x499e70 = _0x466f8e(),
+      _0x43200a = (_0x499e70[_0x49585c(0x12a)] =
+        _0x499e70[_0x49585c(0x12a)] || {}),
+      _0x13e67b = [
+        "log",
+        _0x49585c(0x164),
+        "info",
+        _0x49585c(0x122),
+        _0x49585c(0x154),
+        _0x49585c(0x150),
+        _0x49585c(0x160)
+      ];
+    for (
+      let _0x1bd711 = 0x0;
+      _0x1bd711 < _0x13e67b[_0x49585c(0x163)];
+      _0x1bd711++
+    ) {
+      const _0xddd8c9 = _0x5d357f[_0x49585c(0x12e)][_0x49585c(0x12f)][
+          _0x49585c(0x134)
+        ](_0x5d357f),
+        _0x4027f1 = _0x13e67b[_0x1bd711],
+        _0x3cd559 = _0x43200a[_0x4027f1] || _0xddd8c9;
+      (_0xddd8c9[_0x49585c(0x11f)] = _0x5d357f[_0x49585c(0x134)](_0x5d357f)),
+        (_0xddd8c9[_0x49585c(0x12b)] = _0x3cd559[_0x49585c(0x12b)][
+          _0x49585c(0x134)
+        ](_0x3cd559)),
+        (_0x43200a[_0x4027f1] = _0xddd8c9);
+    }
+  });
+_0x14f97d(),
+  document["addEventListener"](_0x4b0841(0x15c), function () {
+    const _0x506630 = _0x4b0841,
+      _0x32a72a = {};
+    (window[_0x506630(0x153)] = function (_0x191c9c) {
+      const _0x11e599 = _0x506630;
+      Object[_0x11e599(0x135)](_0x32a72a, _0x191c9c);
+    }),
+      setLanguageLabels({
+        en: { days: "D", hours: "H", minutes: "M", seconds: "S" },
+        zh: { days: "天", hours: "时", minutes: "分", seconds: "秒" },
+        hi: {
+          days: _0x506630(0x128),
+          hours: _0x506630(0x142),
+          minutes: _0x506630(0x147),
+          seconds: _0x506630(0x16b)
+        },
+        es: { days: "D", hours: "H", minutes: "M", seconds: "S" },
+        ar: {
+          days: "أيام",
+          hours: _0x506630(0x148),
+          minutes: _0x506630(0x16d),
+          seconds: _0x506630(0x162)
+        },
+        bn: {
+          days: _0x506630(0x125),
+          hours: _0x506630(0x149),
+          minutes: _0x506630(0x16c),
+          seconds: _0x506630(0x158)
+        },
+        pt: { days: "D", hours: "H", minutes: "M", seconds: "S" },
+        ru: { days: "Д", hours: "Ч", minutes: "М", seconds: "С" },
+        ja: { days: "日", hours: "時間", minutes: "分", seconds: "秒" },
+        pa: {
+          days: _0x506630(0x167),
+          hours: _0x506630(0x152),
+          minutes: _0x506630(0x15a),
+          seconds: _0x506630(0x12d)
+        },
+        de: {
+          days: "T",
+          hours: _0x506630(0x143),
+          minutes: _0x506630(0x165),
+          seconds: _0x506630(0x155)
+        },
+        jv: { days: "D", hours: "H", minutes: "M", seconds: "S" },
+        te: {
+          days: _0x506630(0x124),
+          hours: _0x506630(0x144),
+          minutes: "నిమిషాలు",
+          seconds: _0x506630(0x123)
+        },
+        ko: { days: "일", hours: "시간", minutes: "분", seconds: "초" },
+        fr: { days: "J", hours: "H", minutes: "M", seconds: "S" }
+      });
+    const _0x52c2a7 = document[_0x506630(0x131)](_0x506630(0x151));
+    if (!_0x52c2a7) {
+      console[_0x506630(0x122)](_0x506630(0x168));
+      return;
+    }
+    const _0x36ec8e = new Date(_0x52c2a7["getAttribute"](_0x506630(0x137))),
+      _0x592cfc = _0x32a72a["en"];
+    let _0x2e4a89 =
+        _0x52c2a7[_0x506630(0x13e)](_0x506630(0x14c)) ||
+        document[_0x506630(0x16a)][_0x506630(0x13e)](_0x506630(0x14c)) ||
+        "en",
+      _0x1823d5 = _0x32a72a[_0x2e4a89] || _0x592cfc;
+    function _0x9695b3(_0x39d5e1) {
+      const _0x2814dc = _0x506630,
+        _0x424956 = _0x36ec8e - _0x39d5e1;
+      if (_0x424956 <= 0x0) {
+        const _0xa173a3 = _0x52c2a7["getAttribute"](_0x2814dc(0x166));
+        _0x52c2a7[_0x2814dc(0x13d)] = _0xa173a3;
+        return;
+      }
+      let _0x1437ee = Math[_0x2814dc(0x13f)](_0x424956 / 0x3e8);
+      const _0x2484bb = Math[_0x2814dc(0x13f)](
+        _0x1437ee / (0x18 * 0x3c * 0x3c)
+      );
+      _0x1437ee -= _0x2484bb * (0x18 * 0x3c * 0x3c);
+      const _0xc3d25b = Math[_0x2814dc(0x13f)](_0x1437ee / (0x3c * 0x3c));
+      _0x1437ee -= _0xc3d25b * (0x3c * 0x3c);
+      const _0xfeaf4a = Math[_0x2814dc(0x13f)](_0x1437ee / 0x3c),
+        _0x45ba0f = _0x1437ee - _0xfeaf4a * 0x3c;
+      _0x52c2a7["textContent"] =
+        "" +
+        _0x2484bb +
+        _0x1823d5["days"] +
+        "\x20" +
+        String(_0xc3d25b)["padStart"](0x2, "0") +
+        _0x1823d5[_0x2814dc(0x130)] +
+        "\x20" +
+        String(_0xfeaf4a)[_0x2814dc(0x13a)](0x2, "0") +
+        _0x1823d5[_0x2814dc(0x159)] +
+        "\x20" +
+        String(_0x45ba0f)["padStart"](0x2, "0") +
+        _0x1823d5[_0x2814dc(0x169)];
+    }
+    fetch(_0x506630(0x14e))
+      [_0x506630(0x15b)]((_0x29ba4e) => {
+        const _0xdaf0d = _0x506630;
+        if (!_0x29ba4e["ok"])
+          throw new Error(_0xdaf0d(0x156) + _0x29ba4e[_0xdaf0d(0x14f)]);
+        return _0x29ba4e[_0xdaf0d(0x132)]();
+      })
+      [_0x506630(0x15b)]((_0x5008de) => {
+        const _0x5056ea = _0x506630;
+        let _0x50ba0f = new Date(_0x5008de[_0x5056ea(0x121)]);
+        _0x9695b3(_0x50ba0f),
+          setInterval(() => {
+            const _0x5b758a = _0x5056ea;
+            (_0x50ba0f = new Date(_0x50ba0f[_0x5b758a(0x141)]() + 0x3e8)),
+              _0x9695b3(_0x50ba0f);
+          }, 0x3e8);
+      })
+      [_0x506630(0x140)]((_0x22e93b) => {
+        const _0x5e076e = _0x506630;
+        console["error"](_0x5e076e(0x15e), _0x22e93b),
+          (_0x52c2a7[_0x5e076e(0x14d)][_0x5e076e(0x146)] = "none");
+      });
+  });
